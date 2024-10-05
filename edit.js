115c26e7b49ca814b7996c34aad43f3c1be39c6e
shreya <shreyakush06@gmail.com>
2024-10-05 23:34:57 +0530

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,58 @@
+/**toogle icon */
+let menuicon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuicon.onclick=()=>{
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+   
+
+};
+
+
+/*scroll section active link */
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top=window.scrollY;
+        let offset= sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top>=offset&& top<offset +height){
+navlinks.forEach(links=>{
+links.classList.remove('active');
+document.querySelector('header nav a[href*=' +id +']').classList.add('active');
+});
+        };
+    });
+
+    /* Sticky nav bar */
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY >100);
+
+    /*remove navbar and toogle icon */
+    menuicon.classList.remove('bx-x');
+    navbar.classList.remove('bx-x');
+
+
+};
+
+/*scroll revael */
+ScrollReveal({
+     distance:'80px',
+     duration:2000,
+     delay:200
+    });
+    ScrollReveal().reveal('.home-content,.heading', { origin:'top'});
+    ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,contact form', { origin:'bottom'});
+    ScrollReveal().reveal('.home-content h1,about-img', { origin:'left'});
+    ScrollReveal().reveal('.home-content p,about-content', { origin:'right'});
+
+    const typed=new Typed('.multiple-text',{
+String:['Video Editor','Photo Editor','Graphics Designer','Resume Builder'],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop:true
+    });
